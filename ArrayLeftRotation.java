6317b00a3ecea12d8e34d538cef4e4dfb9ced7ba
FKhushbu927 <jannateekhushbu927@gmail.com>
2020-10-20 20:17:04 +0600

    Both ArrayLeftRotation and ArrayRightRotation files are updated again

--- ArrayLeftRotation.java
+++ ArrayLeftRotation.java
@@ -1,27 +1,26 @@
 package ArrayPractice;
 
+/*date 20 oct 2002
+author khushbu */
 import java.util.Arrays;
 
 public class ArrayLeftRotation {
 	public static void main(String[] args) {
 		int A[] = new int[] { 5, 6, 7, 8, 9 };
 		int k = 2, first = 0;
-		  System.out.println("Original array: " + Arrays.toString(A));
+		System.out.println("Original array: " + Arrays.toString(A));
 		for (int i = 0; i < k; i++) {
 			if (A.length > 0) {
 				first = A[A.length - A.length];
 			}
 			for (int j = A.length - A.length; j < A.length - 1; j++) {
-				A[j] = A[j+1];
+				A[j] = A[j + 1];
 			} // end 2nd for loop
 
 			A[A.length - 1] = first;
 		}
-		
-		for(int i=0;i<A.length;i++) {
-			System.out.println(	" "+A[i]);
+		for (int i = 0; i < A.length; i++) {
+			System.out.println(" " + A[i]);
 		}
-
 	}
-
 }
---
+++ ArrayRightRotation.java
@@ -0,0 +1,26 @@
+package ArrayPractice;
+/*date 20 oct 2002
+author khushbu */
+
+public class ArrayRightRotation {
+	public static void main(String args[]) {
+		int A[] = new int[] { 1, 7, 8, 10, 22 };
+		int k = 3, last = 0;
+		for (int i = 0; i < k; i++) {
+			if (A.length > 0) {
+				// store the last element into last
+				last = A[A.length - 1];
+			}
+
+			for (int j = A.length - 1; j > 0; j--) {
+				A[j] = A[j - 1];
+			} // end 2nd for loop
+			A[0] = last;
+
+		} // end 1st for loop
+		System.out.println("Array after right rotation: ");
+		for (int i = 0; i < A.length; i++) {
+			System.out.print(A[i] + " ");
+		}
+	}// end main methdod
+}
